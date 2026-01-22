597478084ccfd6eeb421873a5c4ef9dd6a535ebf
Adekeyeadeniyi <adekeyeimmanuel@gmail.com>
2026-01-22 08:40:24 +0100

    Add exo_15.rb to print a pyramid of hashes based on user-defined steps

---
+++ exo_15.rb
@@ -0,0 +1,9 @@
+print "Enter number of steps (1 - 25): "
+
+steps_count = gets.chomp.to_i
+
+if (1..25).include?(steps_count)
+    (1..steps_count).each do |value|
+        puts "#" * value
+    end
+end
